3ee0000e82459c46a809401d24a9363f9ad5acef
dhamolahedonist <dhamolahedonist>
2023-06-13 07:59:50 +0100

    added read me file

---
+++ README.md
@@ -0,0 +1,31 @@
+## Installation
+
+- Clone repo
+- Install npm packages `npm install`
+- Create `.env` file and copy contents of `.env.example` into it
+- Update env variables in `.env`
+- Start the app by running `npm run start`
+
+## Features
+
+- Users have the ability to create a shortened URL link.
+  -Remove the link from the table.
+  -Generate a barcode for a specific link.
+  -Download the generated barcode.
+
+## License
+
+[MIT](LICENSE)
+
+[appveyor-image]: https://badgen.net/appveyor/ci/dougwilson/express/master?label=windows
+[appveyor-url]: https://ci.appveyor.com/project/dougwilson/express
+[coveralls-image]: https://badgen.net/coveralls/c/github/expressjs/express/master
+[coveralls-url]: https://coveralls.io/r/expressjs/express?branch=master
+[github-actions-ci-image]: https://badgen.net/github/checks/expressjs/express/master?label=linux
+[github-actions-ci-url]: https://github.com/expressjs/express/actions/workflows/ci.yml
+[npm-downloads-image]: https://badgen.net/npm/dm/express
+[npm-downloads-url]: https://npmcharts.com/compare/express?minimal=true
+[npm-install-size-image]: https://badgen.net/packagephobia/install/express
+[npm-install-size-url]: https://packagephobia.com/result?p=express
+[npm-url]: https://npmjs.org/package/express
+[npm-version-image]: https://badgen.net/npm/v/express
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.server = exports.app = void 0;
 const express_1 = __importDefault(require("express"));
@@ -14,20 +16,22 @@ dotenv_1.default.config();
 (0, db_1.default)();
 exports.app = (0, express_1.default)();
 const hostname = Object(process.env.HOSTNAME);
-const port = parseInt(process.env.PORT || '3000');
+const port = parseInt(process.env.PORT || "3000");
 exports.app.use(express_1.default.json());
-exports.app.set('view engine', 'ejs');
+exports.app.set("view engine", "ejs");
 exports.app.set("views", path_1.default.join(__dirname, "views"));
 exports.app.use(express_1.default.urlencoded({ extended: false }));
 const limiter = (0, express_rate_limit_1.default)({
-    windowMs: 60 * 1000,
-    max: 100,
-    handler: (request, response) => {
-        response.redirect('/');
-    }
+  windowMs: 60 * 1000,
+  max: 100,
+  handler: (request, response) => {
+    response.redirect("/");
+  },
 });
 exports.app.use(limiter);
-exports.app.use('/', shortUrlRoutes_1.default);
+exports.app.use("/", shortUrlRoutes_1.default);
 exports.server = exports.app.listen(port, hostname, () => {
-    console.log(`Express server is started at http://${hostname}: ${process.env.PORT}`);
+  console.log(
+    `Express server is started at http://${hostname}: ${process.env.PORT}`
+  );
 });
